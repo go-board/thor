c1f47880e2531b1bbca650c137b52d59030bf41a
lixiaohui <lixiaohui0812@gmail.com>
2020-05-13 19:38:28 +0800

    add document

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -18,6 +18,11 @@ var (
 func ServeLevel(w http.ResponseWriter, r *http.Request) { globalLevel.ServeHTTP(w, r) }
 
 // Initialize do some initial work for log.
+// Info logger will write to <dir>/<namespace>-<service_name>-<service_id>-info.log
+// Error logger will write to <dir>/<namespace>-<service_name>-<service_id>-error.log
+// Each log file size is limited to 1GB,
+// and maximum days of old log to retained in day, the default is 14 days(2 week),
+// currently both file size and max ages not support change by user.
 func Initialize(dir string, namespace, serviceName, serviceId string) {
 	once.Do(func() {
 		cfg := zapcore.EncoderConfig{
