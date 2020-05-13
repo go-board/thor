5608ec3be9591ee9b3fb38474c22afeb8c221823
lixiaohui <lixiaohui0812@gmail.com>
2020-05-13 19:34:08 +0800

    update log initialize & remove default logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,48 +3,59 @@ package logger
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var globalLevel = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+var (
+	globalLevel = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	once        sync.Once
+)
 
-func ServeLogLevel(w http.ResponseWriter, r *http.Request) { globalLevel.ServeHTTP(w, r) }
+func ServeLevel(w http.ResponseWriter, r *http.Request) { globalLevel.ServeHTTP(w, r) }
 
+// Initialize do some initial work for log.
 func Initialize(dir string, namespace, serviceName, serviceId string) {
-	cfg := zapcore.EncoderConfig{
-		EncodeDuration: zapcore.MillisDurationEncoder,
-		EncodeLevel:    zapcore.LowercaseLevelEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
-		EncodeName:     zapcore.FullNameEncoder,
-		EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
-	}
+	once.Do(func() {
+		cfg := zapcore.EncoderConfig{
+			EncodeDuration: zapcore.MillisDurationEncoder,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+			EncodeName:     zapcore.FullNameEncoder,
+			EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
+		}
 
-	infoLogger = initLogger(fmt.Sprintf("%s/%-%s-%s-info.log", dir, namespace, serviceName, serviceId), cfg, globalLevel)
-	zap.ReplaceGlobals(infoLogger)
-	errorLogger = initLogger(fmt.Sprintf("%s/%-%s-%s-error.log", dir, namespace, serviceName, serviceId), cfg, zapcore.ErrorLevel)
+		infoOutput := &lumberjack.Logger{
+			Filename:  fmt.Sprintf("%s/%s-%s-%s-info.log", dir, namespace, serviceName, serviceId),
+			LocalTime: true,
+			MaxSize:   1024,
+			MaxAge:    14,
+		}
+		errorOutput := &lumberjack.Logger{
+			Filename:  fmt.Sprintf("%s/%s-%s-%s-error.log", dir, namespace, serviceName, serviceId),
+			LocalTime: true,
+			MaxSize:   1024,
+			MaxAge:    14,
+		}
+
+		logger := zap.New(
+			zapcore.NewCore(
+				zapcore.NewJSONEncoder(cfg),
+				zapcore.NewMultiWriteSyncer(zapcore.AddSync(infoOutput)),
+				globalLevel,
+			),
+			zap.AddCaller(),
+			zap.AddStacktrace(zapcore.PanicLevel),
+			zap.ErrorOutput(zapcore.AddSync(errorOutput)),
+			zap.Fields(
+				zap.String("namespace", namespace),
+				zap.String("service_name", serviceName),
+				zap.String("service_id", serviceId),
+			),
+		)
+		zap.ReplaceGlobals(logger)
+	})
 }
-
-func initLogger(file string, cfg zapcore.EncoderConfig, lvl zapcore.LevelEnabler) *zap.Logger {
-	l := &lumberjack.Logger{
-		Filename:  file,
-		LocalTime: true,
-		MaxSize:   1024,
-		MaxAge:    14,
-	}
-	logger := zap.New(zapcore.NewCore(
-		zapcore.NewJSONEncoder(cfg),
-		zapcore.NewMultiWriteSyncer(zapcore.AddSync(l)),
-		lvl,
-	))
-	return logger
-}
-
-var errorLogger = zap.L()
-var infoLogger = zap.L()
-
-func Error() *zap.Logger { return errorLogger }
-
-func Info() *zap.Logger { return infoLogger }
